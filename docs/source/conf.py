9b317562fc6635d865dd9d96af61b96bdd5b9904
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-07-10 12:22:15 +0900

    set initial version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'wpt_tools'
 copyright = '2024, t-sasatani'
 author = 't-sasatani'
-release = '0.1.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
