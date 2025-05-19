97369ab6f83cff96abc46b3f676db1923c00a758
t-sasatani <sasatani.dev@gmail.com>
2025-05-19 15:23:24 +0900

    Change to book template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,16 +21,43 @@ release = '0.1.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'myst_parser'
+    'myst_parser',
+    'sphinx_book_theme',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'press'
-#html_static_path = ['_static']
+html_theme = 'sphinx_book_theme'
+
+# Sphinx Book Theme options
+html_theme_options = {
+    "repository_url": "https://github.com/t-sasatani/wpt-tools",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "use_download_button": True,
+    "home_page_in_toc": True,
+    "show_navbar_depth": 2,
+}
+
+# -- Options for autodoc extension ---------------------------------------
+autodoc_member_order = 'bysource'
+autodoc_default_options = {
+    'members': True,
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# -- Options for intersphinx extension -----------------------------------
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+}
+
+# -- Options for todo extension ------------------------------------------
+todo_include_todos = True
